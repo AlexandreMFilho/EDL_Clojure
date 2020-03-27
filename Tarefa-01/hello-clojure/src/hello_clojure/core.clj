080368cddd1338794b88cb3a949bdab12d72abe2
Xande_Mint <alexmaimartfilho@gmail.com>
2020-03-27 17:20:06 -0300

    *Hello Clojure ok e função soma adicionada

--- Tarefa-01/hello-clojure/src/hello_clojure/core.clj
+++ Tarefa-01/hello-clojure/src/hello_clojure/core.clj
@@ -3,4 +3,11 @@
 (defn foo
   "I don't do a whole lot."
   [x]
-  (println x "Hello, World!"))
+  (println x "Hello,Clojure!!!")
+  (defn hello-clj[]
+    (println "Hello""Clojure""!!!"))
+  (defn soma[x y]
+  (+ x y))
+  (hello-clj)
+  (soma[10 25])
+)
