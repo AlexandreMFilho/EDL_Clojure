f63c08853510c0d861a1c209f90cd830cf928544
Xande_Mint <alexmaimartfilho@gmail.com>
2020-03-28 01:59:36 -0300

    *Criação de funcão_*Utilização existentes_*Aprender usar o core direito..

--- Tarefa-01/hello-clojure/project.clj
+++ Tarefa-01/hello-clojure/project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main hello-clojure.core/foo
+  )
   ; :repl-options {:init-ns hello-clojure.core}
-  ; :main hello-clojure.core)
--- Tarefa-01/hello-clojure/src/hello_clojure/core.clj
+++ Tarefa-01/hello-clojure/src/hello_clojure/core.clj
@@ -1,13 +1,25 @@
 (ns hello-clojure.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello,Clojure!!!")
+(defn foo[x]
+  (println x "Hello,Clojure!!!"))
+
   (defn hello-clj[]
     (println "Hello""Clojure""!!!"))
+
   (defn soma[x y]
-  (+ x y))
-  (hello-clj)
-  (soma[10 25])
-)
+    (+ x y))
+  (soma 10 25)
+
+  (defn sub[x y]
+    (- x y))
+    (sub 100 50)
+    (sub -80 50)
+    (sub 80 -50)
+
+  (defn pares[& args]
+    (filter even? args))
+  (pares 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
+
+    (filter even?[1 2 3 4 5 6 7 8 9 10])
+    (map inc[1 2 3 4 5])
+    (reduce + [1 2 3 4 5 6])
